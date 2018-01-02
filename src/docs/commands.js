94a2f477019e3a2d9d4144bbef61cf6c6eff5978
Patricio López Juri <lopezjuripatricio@gmail.com>
2018-01-02 13:43:46 -0300

    Add command generator script

---
+++ src/docs/commands.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const _ = require("lodash");
+
+const coins = require("../data/meta");
+
+async function main() {
+  for (const coin of _.slice(coins, 0, 50)) {
+    process.stdout.write(`${_.toLower(coin["symbol"])} - ${coin["name"]}\n`);
+  }
+}
+
+main();
